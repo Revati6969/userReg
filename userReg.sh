16e6f197e6f5c9e43192ec2006c488d1f65af94e
Revati Tekale <revitekale1910@gmail.com>
2020-03-04 11:19:42 +0530

    ADDED:Last name validation

--- userReg.sh
+++ userReg.sh
@@ -3,11 +3,13 @@
 
 echo "Welcome to user registration"
 
-read -p "Enter first name: " first
+#read -p "Enter first name: " first
+read -p "Enter last name: " last
 
 pattern1="^[A-Z][a-z]{2,}$"
+pattern2="^[A-Z][a-z]{2,}$"
 
-if [[ $first =~ $pattern1 ]]
+if [[ $first =~ $pattern2 ]]
 then
    echo "Valid"
 else
