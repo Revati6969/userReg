a1b0882598a1eb8116406d895b5c8edec9c315fa
Revati Tekale <revitekale1910@gmail.com>
2020-03-04 14:16:08 +0530

    ADDED:validate mobile number

--- userReg.sh
+++ userReg.sh
@@ -1,21 +1,65 @@
 #!/bin/bash -x
 
-shopt -s extglob
-
 echo "Welcome to user registration"
 
-#read -p "Enter first name: " first
-#read -p "Enter last name: " last 
-read -p "Enter email: " email
+function firstName()
+{
+  read -p "Enter first name: " first
+ 
+  pat1="^[A-Z][a-z]{2,}$"
+   
+   if [[ $first =~ $pat1 ]]
+   then
+      echo "Valid"
+   else
+      echo "Not valid"
+   fi
+}
 
-pat1="^[A-Z][a-z]{2,}$"
-pat2="^[A-Z][a-z]{2,}$"
-pat3="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]{1,})?[@]{1}[a-zA-Z0-9]+[.]{1}[a-z]+([.]?[a-z]+)?$"
+function lastName()
+{
+   read -p "Enter last name: " last
 
-if [[ $email =~ $pat3 ]]
-then
-   echo "Valid"
-else
-   echo "Not valid"
-fi
+   pat2="^[A-Z][a-z]{2,}$"
+   
+   if [[ $last =~ $pat2 ]]
+   then
+      echo "Valid"
+   else
+      echo "Not valid"
+   fi
+}
 
+function email()
+{
+   read -p "Enter email: " email
+
+   pat3="^[a-zA-Z]+([+]?[-]?[.]?[_]?[a-zA-Z0-9]+)?[@]{1}[a-z]+[.]{1}[a-z]{2,}([.]?[a-z]+)?$"
+   
+   if [[ $email =~ $pat3 ]]
+   then
+      echo "Valid"
+   else
+      echo "Not valid"
+   fi
+}
+
+function mobileNumber()
+{
+   read -p "Enter mobile number: " mobileNumber
+
+   pat4="[0-9]{2}[ ]{1}[0-9]{10}$"
+   
+   if [[ $mobileNumber =~ $pat4 ]]
+   then 
+      echo "Valid"
+   else
+      echo "Not valid"
+   fi
+}
+
+firstName 
+lastName
+email
+mobileNumber
+mobileNumber 
