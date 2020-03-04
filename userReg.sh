4c522cf58ee6a32061e45c5dd3e8910fe9eb9f37
Revati Tekale <revitekale1910@gmail.com>
2020-03-04 11:12:56 +0530

    ADDED:FirstNameValidation

--- userReg.sh
+++ userReg.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
 echo "Welcome to user registration"
+
+read -p "Enter first name: " first
+
+pat1="^[A-Z][a-z]{2,}$"
+
+if [[ $first =~ $pat1 ]]
+then
+   echo "Valid"
+else
+   echo "Not valid"
+fi
+
